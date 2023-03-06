7c8055f7ecce621116ca6766ea13ab4f461af02c
JohannesDienst-askui <johannes.dienst@askui.com>
2023-03-06 10:33:11 +0100

    Update demo-automation.ts

--- askui-test/demo-automation.ts
+++ askui-test/demo-automation.ts
@@ -1,51 +1,61 @@
 import { aui } from './helper/jest.setup';
 
 describe('jest with askui', () => {
-    xit('annotate', async () => {
+    it('annotate', async () => {
         await aui.annotateInteractively();
     });
 
     it('should fill up the textfields and push buttons', async () => {
+        await aui.click().button().withText('reset').exec();
+        await aui.waitFor(2000).exec();
+        
         // click on the textfield and type characters
         // repeat this as many times as the textfields
-        await aui.click().text().withText('Enter your username').exec();
+        await aui.click().text().withTextRegex('nter[\\s]{0,1}your[\\s]{0,1}usernam').exec();
+        await aui.waitFor(1000).exec();
         await aui.type('askui').exec();
-        await aui.click().text().withText('Enter your email').exec();
-        await aui.type('askui@askui.com').exec();
+        await aui.pressAndroidKey("back").exec();
+
+        await aui.click().text().withTextRegex('nter[\\s]{0,1}your[\\s]{0,1}emai').exec();
+        await aui.waitFor(1000).exec();
+        await aui.type('johannes.dienst@askui.com').exec();
+        await aui.pressAndroidKey("back").exec();
+        
         // Click and type the address
-        await aui.click().text().withText('Enter your address').exec();
-        await aui.type('Haid-und-Neu-Straße 18').exec();
-        // Pressing enter is the equivelant to pressing the return button on the on-screen-keyboard
+        await aui.click().text().withTextRegex('nter[\\s]{0,1}your[\\s]{0,1}addres').exec();
+        await aui.waitFor(1000).exec();
+        await aui.type('Emmy-Noether-Strasse 17').exec();
+        await aui.pressAndroidKey("back").exec();
+
+        // Pressing enter is the equivalent to pressing the return button on the on-screen-keyboard
         // This gets rid of the focus from the textfield
         await aui.pressAndroidKey('enter').exec();
+        
         // Press the 'Submit' button
-        await aui.click().text().withText('Submit').exec();
+        await aui.click().button().withText('Submit').exec();
+        
         // We will have a popup window that has two buttons. Press the 'Refuse' button
         await aui.click().text().withText('Refuse').exec();
-        // Here we press multiple of toggle buttons one by one
+
+        // Here we press multiple toggle buttons one by one
         await aui.click().text().withText('Banana').exec();
         await aui.click().text().withText('Mango').exec();
         await aui.click().text().withText('Sunny').exec();
         await aui.click().text().withText('Rainy').exec();
         await aui.click().text().withText('Windy').exec();
-        // Attention for swiping!
-        /*  Swipe/scroll within the page
-            - execOnShell() can run shell commands within the device via adb.
-            - Note that, you have to adjust the four numeric parameters,
-              in order to make it fit to your device's screen.
-            - The syntax is:
-                input swipe <startX> <startY> <endX> <endY>
-            - Depending on the screen size of your device,
-              the coordinates should stay within the scrollable/swipeable area of the app.
-              i.e. the 'Tabbar' at the top of the demo app is not scrollable.
-        */
-        // Here we swipe the page two times in a row
-        await aui.execOnShell('input swipe 1000 1000 100 1000').exec();
-        await aui.execOnShell('input swipe 1000 1000 100 1000').exec();
+        
+        // Fuzzy
+        await aui.click().button().withText('Submit').exec();
+        await aui.click().text().withText('Refuse').exec();
+        
+        await aui.moveMouseRelativelyTo(0, 100).text().withText("Windy").exec();
+        await aui.mouseToggleDown().exec();
+        await aui.moveMouseRelatively(-1500, 0).exec();
+        await aui.mouseToggleUp().exec();
     });
 
 
-    it('should pick the dates', async () => {
+    xit('should pick the dates', async () => {
         // First, we type in the desired values into the textfields.
         await aui.click().text().withText('Title').exec();
         await aui.type('My vacation plan').exec();
@@ -75,7 +85,7 @@ describe('jest with askui', () => {
     
     });
 
-    it('should take a picture', async ()=>{
+    xit('should take a picture', async ()=>{
         // Click on the button 'Take a Picture', then it will launch the camera
         await aui.click().button().contains().text().withText('Take a Picture').exec();
         // Notice how we select the record button.
